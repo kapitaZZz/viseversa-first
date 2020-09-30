5dca6d2644dc6db77624dbb2beeb5bf41ede3ce5
kapitazzz <agarussky@gmail.com>
2020-09-30 16:00:58 +0300

    Add count of words in usertext

--- viseversa_project/views.py
+++ viseversa_project/views.py
@@ -8,5 +8,10 @@ def home(request):
 def reverse(request):
 	usertext = request.GET['usertext']
 	reversed_text = usertext[::-1]
+	words_in_string = usertext.split()
+	temp_len = len(words_in_string)
+	print(words_in_string)
+	print(temp_len)
 	return render(request, 'reverse.html', {'usertext': usertext,
-											'reversed_text': reversed_text})
+											'reversed_text': reversed_text, 
+											'words_in_string': temp_len})
